502bb925440f2bf5a22092751ad3d964a39bd9bc
Carol Junqueira <junqueiracarolina@live.com>
2017-11-15 17:21:12 -0200

    teste3

--- app/js/app.js
+++ app/js/app.js
@@ -1,65 +1,19 @@
-var fs = require('fs');
-var util = require('util')
-var exec = require('child_process').execSync;
-
-var macs = [];
-
-function puts(error, stdout, stderr) { util.puts(stdout) }
+const Repeat = require('repeat');
+const  exec  = require('child-process-promise').exec;
 
 
-/**
- * @description
- * Função para ler o arquivo linha por linha (registro)
- *
- * @param {input} arquivo a ser livo
- *   @type {func} checkMacAddress
- */
-function readLines(input, func) {
-  // é o que ainda resta para ler do arquivo
-  var remaining = '';
+var count = 0;
+function scan() {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth(); 
+  let year   = date.getFullYear();
+  let hour = date.getHours();
+  let fileName = 'LTIA_'+year+'-'+month+'-'+day+'_'+hour+'-00';
+  console.log(fileName);
+  exec('ping google.com').then(console.log('terminei'));
 
-  input.on('data', function(data) {
-    remaining += data;
-    var index = remaining.indexOf('\n');
-    while (index > -1) {
-      var line = remaining.substring(0, index);
-      remaining = remaining.substring(index + 1);
-      checkMacAddress(line);
-      index = remaining.indexOf('\n');
-    }
-
-  });
-
-  input.on('end', function() {
-    if (remaining.length > 0) {
-      checkMacAddress(remaining);
-    }else{
-      console.log("Por aqui há " + macs.length + " pessoas");
-    }
-  });
+  
 }
-
-/**
- * @description
- * Função para ler o arquivo linha por linha (registro)
- *
- * @param {input} arquivo a ser livo
- *   @type {func} checkMacAddress
- */
-
-function checkMacAddress(data) {
-  var array = data.split("\t");
-  if (macs.indexOf(array[0]) == -1 && array[0] != ""){
-      macs.push(array[0]);
-  }
-}
-module.exports.numPeople = function(file){
-  //exec("ifconfig wlan0 down",puts);
-  //exec("iwconfig wlan0 mode monitor",puts);
-  //exec("ifconfig wlan0 up",puts);
-
-  //exec("tshark -i wlan0",puts);
-  var input = fs.createReadStream(file);
-  readLines(input, checkMacAddress);
-  //exec("rm 'output.txt'",puts);
-}
+ 
+scan();
