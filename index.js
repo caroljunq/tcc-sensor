f375dcaa0f3826befd7b5ac5efe15fa7aaf06774
jdgr81 <julianadag@gmail.com>
2017-11-16 00:19:17 +0000

    indexfinal

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ function scan() {
   let month = date.getMonth(); 
   let year   = date.getFullYear();
   let hour = date.getHours();
-  let fileName = 'LTIA_'+year+'-'+month+'-'+day+'_'+hour+':'+'00'; 
+  let fileName = 'LTIA_'+year+'-'+month+'-'+day+'_'+hour+':00'; 
 
   exec('tshark -i wlan1 -a duration:18 -Y "wlan.fc.type_subtype eq 4" -T fields -e wlan.sa >'+ fileName).then( function (result){
   fs.readFile(fileName, "utf8", (err,data) => {
     let formData = {};
     formData.file = data;
     formData.fileName = fileName;
-    request.post({url: '', formData: formData}, function(err,res,body){console.log(body)});
+    request.post({url: 'http://', formData: formData}, function(err,res,body){console.log(body)});
+    
   });
 });	
 }
