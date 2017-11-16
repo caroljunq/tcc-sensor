e144fe18aa2191785c3ba9a027e1b1c809ed76cf
Carol Junqueira <junqueiracarolina@live.com>
2017-11-15 23:47:06 -0200

    correcao nome arq

--- index.js
+++ index.js
@@ -8,23 +8,22 @@ exec('ifconfig wlan1 down')
          .then(exec('ifconfig wlan1 up')));
 
 function scan() {
-
   let date = new Date();
   let day = date.getDate();
   let month = date.getMonth(); 
   let year   = date.getFullYear();
   let hour = date.getHours();
-  let fileName = 'LTIA_'+year+'-'+month+'-'+day+'_'+hour+':00'; 
+  let fileName = 'LTIA_'+year+'-'+month+'-'+day+'_'+hour+'-00';
 
-  exec('tshark -i wlan1 -a duration:18 -Y "wlan.fc.type_subtype eq 4" -T fields -e wlan.sa >'+ fileName).then( function (result){
-  fs.readFile(fileName, "utf8", (err,data) => {
-    let formData = {};
-    formData.file = data;
-    formData.fileName = fileName;
-    request.post({url: 'http://', formData: formData}, function(err,res,body){console.log(body)});
-    
-  });
-});	
+  exec('tshark -i wlan1 -a duration:18 -Y "wlan.fc.type_subtype eq 4" -T fields -e wlan.sa >'+ fileName)
+    .then((result) => {
+      fs.readFile(fileName, "utf8", (err,data) => {
+        let formData = {};
+        formData.file = data;
+        formData.fileName = fileName;
+        request.post({url: 'http://', formData: formData}, (err,res,body) => {console.log(body)});     
+      });
+    });	
 }
 
 Repeat(scan).every(20, 'sec').for(2, 'minutes').start.in(1, 'sec');
