4b412d28dd4b0eeb0651eb3dc0db93b707d07372
Carol Junqueira <junqueiracarolina@live.com>
2017-11-20 20:29:55 -0200

    teste processos

--- index.js
+++ index.js
@@ -2,59 +2,79 @@ const Repeat = require('repeat');
 const exec = require('child-process-promise').exec;
 const request = require('request');
 const fs = require('fs');
+const events = require('events');
+
+let eventEmitter = new events.EventEmitter();
+
+let fileText = '';
 
 exec('ifconfig wlan1 down')
     .then(exec('iwconfig wlan1 mode monitor')
          .then(exec('ifconfig wlan1 up')));
 
 
-function sendFiles(){
-  fs.readdir('./', (err, files) => {
-    files.forEach(file =>{
-      let name = file.split('_');
-      if(name[0] == 'not-sent'){
-        fs.readFile(file, "utf8", (err, data) =>{
-          let formData = {};
-          formData.file = data;
-          formData.fileName = name[1]+'_'+name[2]+'_'+name[3];
-          request.post({url:"http://",formData: formData}, (err, res,body) => {
-            if(!err)
-              fs.rename(file,formData.fileName, (err) => {});
-          });
+///read all files in directory.If file name is the same that prefix, then try to send file to server.
+function sendFiles(prefix){
+    fs.readdir('./', (err, files) => {
+        files.forEach(file =>{
+            let name = file.split('_');
+            if(name[0] == prefix){
+                fs.readFile(file, "utf8", (err, data) =>{
+                    let formData = {};
+                    formData.file = data;
+                    formData.fileName = name[1]+'_'+name[2]+'_'+name[3];
+                    request.post({url:"http://",formData}, (err, res, body) =>{
+                        if(!err){
+                            fs.rename(file, 'sent_'+file, (err) => {});
+                        }else{
+                            fs.rename(file,'not-sent_'+file, (err) =>{})
+                        }
+                    });
+                });
+            }
         });
-      }
-    })
-  });
+    });
+}
+
+function createFile(){
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year   = date.getFullYear();
+    let hour = date.getHours();
+    if(hour < 10){
+      hour = '0'+hour;
+    }
+    let fileName = 'LTIA_'+year+'-'+month+'-'+day+'_'+hour+'-00';
+
+    fs.writeFile(fileName, fileText, (err) => {
+        if(err) {
+            return console.log(err);
+        }
+        fileText = '';
+        eventEmitter.emit('fileCreated');
+        repeatScan();
+    });
 }
 
 function scan() {
-  sendFiles();
-  let date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth() + 1; 
-  let year   = date.getFullYear();
-  let hour = date.getHours();
-  if(hour < 10){
-    hour = '0'+hour;
-  }
-  let fileName = 'LTIA_'+year+'-'+month+'-'+day+'_'+hour+'-00';
-
-  exec('tshark -i wlan1 -a duration:18 -Y "wlan.fc.type_subtype eq 4" -T fields -e wlan.sa >'+ fileName)
+  exec('tshark -i wlan1 -Y "wlan.fc.type_subtype eq 4" -T fields -e wlan.sa')
     .then((result) => {
-      fs.readFile(fileName, "utf8", (err,data) => {
-        let formData = {};
-        formData.file = data;
-        formData.fileName = fileName;
-        request.post({url: 'http://', formData: formData}, (err,res,body) => {
-          if(err){
-            fs.rename(fileName,'not-sent_'+fileName, (err) =>{})
-          }
-        });     
-      });
-    });	
+        fileText += result.stdout;
+    }).catch((err) => {
+        return;
+    });
 }
 
-Repeat(scan).every(20, 'sec').for(2, 'minutes').start.in(1, 'sec');
+//repeat Tshark process
+function repeatScan(){
+    Repeat(scan).every(20, 'sec').for(3200, 'minutes').start.now(); //every 20 seconds
+}
 
+eventEmitter.on('fileCreated', () => {sendFiles('LTIA')});
 
+Repeat(createFile).every(3400, 'sec').for(2, 'minutes').start.wait(3300, 'sec'); //every 1 hour, for X Hours
 
+Repeat(() => {sendFiles('not-sent')}).every(1200, 'sec').for(2, 'minutes').start.now(); // every 30 minutes, for x hours
+
+repeatScan();
